c6f0fa5630b49158ec986a2d5e5ac8b460455f62
TahshinSharon <atsharon88@gmail.com>
2025-07-31 01:28:57 +0600

    Singleton Pattern In Java Language With Best practice

--- Singleton-Pattern/SingletonExample.java
+++ Singleton-Pattern/SingletonExample.java
@@ -5,23 +5,24 @@ public class SingletonExample {
     // Step 1: Create a private static instance of the same class
     private static volatile SingletonExample instance;
     private String data;
+
     // Step 2: Make the constructor private to prevent external instantiation
     private SingletonExample(String data) {
-        this.data =data;
+        this.data = data;
         System.out.println(this.data);
     }
 
     // Step 3: Create a public static method to return the instance
     public static SingletonExample getInstance(String data) {
-        if(instance == null){
-        synchronized(SingletonExample.class){
-             if (instance == null) {
-            instance = new SingletonExample(data);
+        if (instance == null) {
+            synchronized (SingletonExample.class) {
+                if (instance == null) {
+                    instance = new SingletonExample(data);
+                }
+            }
         }
+        return instance;
     }
-  }
-  return instance;
-}
 
     // Example method
     public void showMessage() {
@@ -32,7 +33,10 @@ public class SingletonExample {
     public static void main(String[] args) {
         // Try to create multiple instances
         SingletonExample obj1 = SingletonExample.getInstance("Singleton Instance Created");
-        SingletonExample obj2 = SingletonExample.getInstance("2nd Singleton Instance Created");// this string should not be printted cause Instance is already created
+        SingletonExample obj2 = SingletonExample.getInstance("2nd Singleton Instance Created");// this string should not
+                                                                                               // be printted cause
+                                                                                               // Instance is already
+                                                                                               // created
 
         // Check if both objects are the same
         System.out.println("obj1 hash: " + obj1.hashCode());
